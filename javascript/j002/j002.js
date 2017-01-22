6c6e89b73bf27c0320f1e1ba60075ba4c0198cd8
Jose <jrivera4@uwo.ca>
2017-01-22 02:18:29 -0500

    time j002 32

--- javascript/j002/j002.js
+++ javascript/j002/j002.js
@@ -9,4 +9,12 @@ function answer(){
 	document.getElementById('answer_space').innerHTML = "Answer: " + examdatabase.questions[0].answer;
 }
 
-
+function correct(){
+	 var textBox = document.getElementById("correct_box");
+     textBox.value++;
+}
+	
+function incorrect(){
+	var textBox = document.getElementById('incorrect_box');
+	textBox.value++;
+}
