b531223344553e33c9a315515f586c6a2f8aa94d
Jose Rivera <jrivera4@uwo.ca>
2017-01-28 22:17:26 -0500

    time r001 44

--- ruby/r001/r001.rb
+++ ruby/r001/r001.rb
@@ -4,9 +4,26 @@
 
 
 
-puts 'Hello, world!'
+puts 'Now processing....'
+puts 'You can find the ouput file in the relative directory.'
 
-File.open("logout.txt").each do |line|
-	puts line
+fname = "r001out.txt"
+
+#When passing parameter to .open, "w" overwrites current file while 
+#"a" appends to it and "r" reads
+outputfile = File.open(fname, "w")
+input = File.open("logout.txt").each do |line|
+	
+		if line["Bob"] || line["framework"]
+			line.chomp!
+		else
+			outputfile.puts line
+		end
+	
 end
+outputfile.close
+input.close
+
+#TO DO: Devlop Code to group code and delete based on author
+#TO DO: Develop code to ouput straight to HTML file
 
