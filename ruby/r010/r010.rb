ddf52d0362074e270dd8454322bdd7e91a186442
Jose Rivera <jrivera4@uwo.ca>
2017-02-12 03:33:01 -0500

    time r010 66

---
+++ ruby/r010/r010.rb
@@ -0,0 +1,28 @@
+#Ruby Task: r010
+#Author: Jose Rivera
+#CS 3342
+
+
+fname = "output.txt"
+
+outputfile = File.open(fname, "w")
+
+inputs = Array.new(6)
+		
+input = File.open("input.txt").each do |line|
+
+		if line[/\d/]
+                        string = line.strip
+                        num = string.to_i
+			outputfile.puts "i  #{num}"
+		else
+			outputfile.puts line
+		end
+	
+end
+outputfile.close
+input.close
+
+#Notes to consider:
+#Right now the program works if the numbers are on seperate lines
+#Also the program must be able to distinguish integers and floats
