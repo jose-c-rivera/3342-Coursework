95763ddee284c737c4e7fb4e3b5f5af37799e772
Jose Rivera <jrivera4@uwo.ca>
2017-02-17 11:08:31 -0500

    time j003 28

--- javascript/j003/j003.js
+++ javascript/j003/j003.js
@@ -11,13 +11,19 @@ function answer(question){
 }
 
 function correct(){
-	 var textBox = document.getElementById("correct_box");
-     textBox.value++;
+	var textBox = document.getElementById("correct_box");
+     	textBox.value++;
 }
 	
 function incorrect(){
 	var textBox = document.getElementById('incorrect_box');
 	textBox.value++;
+}
+
+function undo(){
+	var textBox = document.getElementById('incorrect_box');
+	if(textBox.value != 0)	
+		textBox.value--;
 }
 
 function newQ(){
