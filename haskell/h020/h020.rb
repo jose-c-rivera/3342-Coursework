7a007c47f09d41d1e6fce943fb37393cc158aeeb
Jose Rivera <jrivera4@uwo.ca>
2017-03-10 10:37:11 -0500

    time h020 24 Created frame of parser, needs work*

---
+++ haskell/h020/h020.rb
@@ -0,0 +1,26 @@
+--Title: Haskell h020
+--Author: Jose Carlos Rivera
+--CS 3342
+
+import System.IO
+import System.Process
+
+main :: IO()
+main = do
+
+
+input <- openFile "input.txt" ReadMode
+output <- openFile "output.txt" WriteMode
+
+mainloop input output
+hClose input
+hClose output
+
+mainloop :: Handle -> Handle -> IO ()
+mainloop input output =
+do ineof <- hIsEOF input
+       if ineof
+           then return ()
+           else do inpStr <- hGetLine input
+                   hPutStrLn output (map toUpper inpStr)
+                   mainloop input output
